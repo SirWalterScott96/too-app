97524a87d591405ea64edfa53fe577b1a435eb4a
ardit <dpfrjyt@gmail.com>
2023-07-22 18:34:34 +0300

    Implemented add button

--- function.py
+++ function.py
@@ -1,10 +1,17 @@
-def get_todos(filepath):
-    with open(filepath) as file_local:
+FILEPATH = 'files/todos.txt'
+
+def get_todos(filepath=FILEPATH):
+    with open(FILEPATH) as file_local:
         todos_local = file_local.readlines()
 
         return todos_local
 
 
-def write_todos(filepath, todos_local):
-    with open(filepath, 'w') as file_local:
+def write_todos(todos_local,filepath=FILEPATH):
+    with open(FILEPATH, 'w') as file_local:
         file_local.writelines(todos_local)
+
+
+
+if __name__ == '__main__':
+    get_todos()
--- gui.py
+++ gui.py
@@ -1,13 +1,27 @@
 import function
-import PySimpleGUI as Ps
+import PySimpleGUI as sg
 
-label = Ps.Text('Type in a to-do')
-input_box = Ps.InputText(tooltip='Enter To-Do')
-add_button = Ps.Button('Add')
+label = sg.Text('Type in a to-do')
+input_box = sg.InputText(tooltip='Enter To-Do', key='todo')
+add_button = sg.Button('Add')
 
-window = Ps.Window('My To-Do App', layout=[[label], [input_box, add_button]])
+window = sg.Window('My To-Do App',
+                   layout=[[label], [input_box, add_button]],
+                   font=('Helvetica', 20))
 
-window.read()
+while True:
+    event, values = window.read()
+    print(event)
+    print(values)
+    match event:
+        case 'Add':
+            todos = function.get_todos()
+            new_todo = values['todo'] + '\n'
+            todos.append(new_todo)
+            function.write_todos(todos)
+        case sg.WINDOW_CLOSED:
+            break
+
 
 window.close()
 
